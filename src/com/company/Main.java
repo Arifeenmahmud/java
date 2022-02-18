7e6ac0974ac9a03e0ec778b9fccc23f193731c91
Arifeen Mahmud <arifeen.mahmud@yahoo.com>
2022-02-19 02:04:00 +0600

    large low values

---
+++ src/com/company/Main.java
@@ -0,0 +1,37 @@
+package com.company;
+
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your first number");
+        double num1 = sc.nextDouble();
+        System.out.println("Enter your second number");
+        double num2 = sc.nextDouble();
+        System.out.println("Enter your third number");
+        double num3 = sc.nextDouble();
+        double largest;
+        double lowest;
+        if (num1 >= num2 && num1 >= num3) {
+            largest = num1;
+        } else if (num2 >= num1 && num2 >= num3) {
+            largest = num2;
+        } else {
+            largest = num3;
+        }
+        if (num1 <= num2 && num1 <= num3) {
+            lowest = num1;
+        } else if (num2 <= num1 && num2 <= num3) {
+            lowest = num2;
+        } else {
+            lowest = num3;
+        }
+        System.out.println("The largest number is " + largest);
+        System.out.println("Largest Number is " + largest + "\nLowest number is " + lowest);
+    }
+}
