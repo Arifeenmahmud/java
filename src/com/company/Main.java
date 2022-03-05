7ae4ad21c043e20d49831649bd0bef5bfe3751b3
Arifeen Mahmud <arifeen.mahmud@yahoo.com>
2022-03-06 00:53:38 +0600

    Large Low User Input Range

---
+++ src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+
+        int i, n, lar,sm, elem;
+        System.out.printf ("Enter total number of elements ");
+        elem=sc.nextInt();
+
+        System.out.printf ("Enter first number ");
+        n=sc.nextInt();
+
+        lar = n;
+        sm=n;
+        for (i=1; i<= elem -1 ; i++)
+        {
+            System.out.printf ("Enter another number ");
+
+            n=sc.nextInt();
+            if (n>lar)
+                lar=n;
+            if (n<sm)
+                sm=n;
+        }
+        System.out.printf ("The largest number is %d", lar);
+        System.out.printf (" and The smallest number is %d \n", sm);
+
+    }
+}
