3a290e58ccdf738a275f2c547dc8df34cba8a01e
Arifeen Mahmud <arifeen.mahmud@yahoo.com>
2022-03-20 16:56:04 +0600

    File Write and Read

--- src/WriteToFile.java
+++ src/WriteToFile.java
@@ -4,18 +4,18 @@ import java.io.*;
 
 public class WriteToFile {
     public static void main(String[] args) throws FileNotFoundException {
-        try {
-            FileWriter myWriter = new FileWriter("D:\\filename.txt");
-            myWriter.write("This is Ok Now Files in Java might be tricky, but it is fun enough! ");
-            myWriter.close();
-            System.out.println("Successfully wrote to the file.");
-        } catch (IOException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
+//        try {
+//            FileWriter myWriter = new FileWriter("D:\\filename.txt");
+//            myWriter.write("This is Ok Now Files in Java might be tricky, but it is fun enough! ");
+//            myWriter.close();
+//            System.out.println("Successfully wrote to the file.");
+//        } catch (IOException e) {
+//            System.out.println("An error occurred.");
+//            e.printStackTrace();
+//        }
 
         File file = new File("D:\\filename.txt");
-        System.out.println(file.exists());
+//        System.out.println(file.exists());
         BufferedReader br = new BufferedReader(new FileReader(file));
         String st;
         try {
