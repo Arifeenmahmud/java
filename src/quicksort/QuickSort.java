792402a0ee176c93909ae58a7073bc00f506dd78
Arifeen Mahmud <arifeen.mahmud@yahoo.com>
2022-03-29 18:51:39 +0600

    Quick Sort

---
+++ src/quicksort/QuickSort.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quicksort;
+
+/**
+ *
+ * @author Arifeen
+ */
+public class QuickSort {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int[] list = {2, 3, 2, 5, 6, 1, -2, 3, 14, 12};
+        quickSort(list);
+        for (int i = 0; i < list.length; i++) {
+            int j = list[i];
+            System.out.print(" " + j);
+        }
+
+    }
+    public static void quickSort(int[] list) {
+        quickSort(list, 0, list.length - 1);
+    }
+    public static void quickSort(int[] list, int first, int last){
+        if (last > first) {
+            int pivotIndex = partition(list, first, last);
+            quickSort(list, first, pivotIndex - 1);
+            quickSort(list, pivotIndex + 1, last);
+        }
+    }
+    public static int partition(int[] list, int first, int last) {
+        int pivot = list[first];
+        int low = first + 1;
+        int high = last;
+        while (high > low) {
+            while (low <= high && list[low] <= pivot)
+                low++;
+            while (low <= high && list[high] > pivot)
+                high--;
+            while (low <= high && list[high] > pivot)
+                high--;
+            if (high > low) {
+                int temp = list[high];
+                list[high] = list[low];
+                list[low] = temp;
+
+            }
+        }
+        while (high > first && list[high] >= pivot)
+            high--;
+        if (pivot > list[high]) {
+            list[first] = list[high];
+            list[high] = pivot;
+            return high;
+
+        }else {
+            return first;
+        }
+    }
+
+}
