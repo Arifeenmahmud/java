b0a94bfaa37ba5d7fed3d08d2d54ab383a6ee63b
Arifeen Mahmud <arifeen.mahmud@yahoo.com>
2022-04-04 15:35:28 +0600

    Check add value correcto or wrong

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        int number1 = (int)(Math.random() * 10);
+        int number2 = (int)(Math.random() * 10);
+
+        Scanner input = new Scanner(System.in);
+        System.out.print( "What is " + number1 + " + " + number2 + "? ");
+        int answer = input.nextInt();
+
+        while (number1 + number2 != answer) {
+            System.out.print("Wrong answer. Try again. What is + number1 + "+ " + number2 " + "?" );
+            answer = input.nextInt();
+        }
+        System.out.println("You got it!");
+    }
+}
